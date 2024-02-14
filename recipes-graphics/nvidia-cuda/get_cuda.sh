b3ef3f590ad0af17e3c6e0478d7cdeff93caff64
Nauman3S <naumanshakir3s@gmail.com>
2024-02-14 10:55:23 +0100

    fixed cuda installer

---
+++ recipes-graphics/nvidia-cuda/get_cuda.sh
@@ -0,0 +1,11 @@
+PV = "12.0.1"
+PR = "r0"
+
+SRC_URI = "https://developer.download.nvidia.com/compute/cuda/${PV}/local_installers/cuda_${PV}_525.85.12_linux.run"
+
+wget -P /opt/ ${SRC_URI}
+chmod +x /opt/cuda_12.0.1_525.85.12_linux.run
+
+echo "extracting cuda"
+cd /opt
+./cuda_12.0.1_525.85.12_linux.run --silent --toolkit --samples --installpath=/opt/cuda-install
