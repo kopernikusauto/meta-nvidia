8e3cda9d6660f0f7a79a2d06f5054746112eeb4f
Nauman3S <naumanshakir3s@gmail.com>
2024-02-14 10:58:44 +0100

    fixed vars

--- recipes-graphics/nvidia-cuda/get_cuda.sh
+++ recipes-graphics/nvidia-cuda/get_cuda.sh
@@ -1,7 +1,7 @@
-PV = "12.0.1"
-PR = "r0"
+local PV = "12.0.1"
+local PR = "r0"
 
-SRC_URI = "https://developer.download.nvidia.com/compute/cuda/$PV/local_installers/cuda_$PV_525.85.12_linux.run"
+local SRC_URI = "https://developer.download.nvidia.com/compute/cuda/$PV/local_installers/cuda_$PV_525.85.12_linux.run"
 
 wget -P /opt/ $SRC_URI
 chmod +x /opt/cuda_12.0.1_525.85.12_linux.run
